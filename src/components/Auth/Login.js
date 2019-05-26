b6500ea67e540602f380c7398c3bfb5480b8de6c
Nick Pizzo <nick.pizzo@gmail.com>
2019-05-26 16:40:49 -0700

    sign up and sign in users

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import useFormValidation from "./useFormValidation";
 import validateLogin from "./validateLogin";
+import firebase from "../../firebase";
 
 const INITIAL_STATE = {
   name: "",
@@ -16,13 +17,21 @@ function Login(props) {
     values,
     errors,
     isSubmitting
-  } = useFormValidation(INITIAL_STATE, validateLogin);
+  } = useFormValidation(INITIAL_STATE, validateLogin, authenticateUser);
   const [login, setLogin] = useState(true);
+
+  async function authenticateUser() {
+    const { name, email, password } = values;
+    const response = login
+      ? await firebase.login(email, password)
+      : await firebase.register(name, email, password);
+    console.log({ response });
+  }
 
   return (
     <div>
       <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
-      <form onSubmit={handleSubmit} className="flex flex-column" action="">
+      <form onSubmit={handleSubmit} className="flex flex-column">
         {!login && (
           <input
             name="name"
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-function useFormValidation(initialState, validate) {
+function useFormValidation(initialState, validate, authenticate) {
   const [values, setValues] = useState(initialState);
   const [errors, setErrors] = useState({});
   const [isSubmitting, setSubmitting] = useState(false);
@@ -9,7 +9,7 @@ function useFormValidation(initialState, validate) {
     if (isSubmitting) {
       const noErrors = Object.keys(errors).length === 0;
       if (noErrors) {
-        console.log("authenticated");
+        authenticate();
         setSubmitting(false);
       } else {
         setSubmitting(false);
@@ -34,7 +34,7 @@ function useFormValidation(initialState, validate) {
     event.preventDefault();
     const validationErrors = validate(values);
     setErrors(validationErrors);
-    console.log({ values });
+    setSubmitting(true);
   }
 
   return {
