ea1d00e839de45685e70899ed52e3c02bc2bd4d5
Nick Pizzo <nick.pizzo@gmail.com>
2019-05-27 12:22:49 -0700

    logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,18 +25,24 @@ function Header() {
         <NavLink to="/search" className="header-link">
           search
         </NavLink>
-        <div className="divider">|</div>
+        {user && (
+          <>
+            <div className="divider">|</div>
 
-        <NavLink to="/create" className="header-link">
-          submit
-        </NavLink>
+            <NavLink to="/create" className="header-link">
+              submit
+            </NavLink>
+          </>
+        )}
       </div>
       <div className="flex">
         {user ? (
           <>
             <div className="header-name">{user.displayName}</div>
             <div className="divider">|</div>
-            <div className="header-button">Logout</div>
+            <div className="header-button" onClick={() => firebase.logout()}>
+              Logout
+            </div>
           </>
         ) : (
           <NavLink to="/login" className="header-link">
