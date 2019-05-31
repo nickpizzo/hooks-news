561de1a9a72d84b5cc420e39770c046aaf1accdd
Nick Pizzo <nick.pizzo@gmail.com>
2019-05-31 11:26:30 -0700

    deploy w firebase hosting

---
+++ build/precache-manifest.1dc159955d821ea10c05abd51cffe437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2096f1bcca249109ed74",
+    "url": "/static/js/main.c68c916d.chunk.js"
+  },
+  {
+    "revision": "bdd95fc765606cd25e37",
+    "url": "/static/js/2.ad865be8.chunk.js"
+  },
+  {
+    "revision": "2096f1bcca249109ed74",
+    "url": "/static/css/main.60db92bd.chunk.css"
+  },
+  {
+    "revision": "b0153b9885ad58f4ea853946de53a045",
+    "url": "/index.html"
+  }
+];
