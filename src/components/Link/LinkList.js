7f9a0938b7cb1b7fdb9e2307fa955b530848e24a
Nick Pizzo <nick.pizzo@gmail.com>
2019-05-31 10:40:04 -0700

    cursor based pagination, pagination cloud function

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,29 +1,57 @@
 import React, { useState, useEffect, useContext } from "react";
 import { FirebaseContext } from "../../firebase";
 import LinkItem from "./LinkItem";
+import { LINKS_PER_PAGE } from "../../utils/index";
+import axios from "axios";
 
 function LinkList(props) {
   const { firebase } = useContext(FirebaseContext);
   const [links, setLinks] = useState([]);
-  // const isNewPage = props.location.pathname.includes("new");
+  const [cursor, setCursor] = useState(null);
+  const isNewPage = props.location.pathname.includes("new");
   const isTopPage = props.location.pathname.includes("top");
+  const page = Number(props.match.params.page);
 
   useEffect(() => {
     const unsubscribe = getLinks();
     return () => unsubscribe();
-  }, [isTopPage]);
+  }, [isTopPage, page]);
 
   function getLinks() {
+    const hasCursor = Boolean(cursor);
     if (isTopPage) {
       return firebase.db
         .collection("links")
         .orderBy("voteCount", "desc")
+        .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
+    } else if (page === 1) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (hasCursor) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .startAfter(cursor.created)
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else {
+      const offset = page * LINKS_PER_PAGE - LINKS_PER_PAGE;
+      axios
+        .get(
+          `https://us-central1-hooks-news-app-d6f09.cloudfunctions.net/linksPagination?offset=${offset}`
+        )
+        .then(response => {
+          const links = response.data;
+          const lastLink = links[links.length - 1];
+          setLinks(links);
+          setCursor(lastLink);
+        });
+      return () => {};
     }
-    return firebase.db
-      .collection("links")
-      .orderBy("created", "desc")
-      .onSnapshot(handleSnapshot);
   }
 
   function handleSnapshot(snapshot) {
@@ -31,7 +59,23 @@ function LinkList(props) {
       return { id: doc.id, ...doc.data() };
     });
     setLinks(links);
+    const lastLink = links[links.length - 1];
+    setCursor(lastLink);
   }
+
+  function visitPreviousPage() {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`);
+    }
+  }
+
+  function visitNextPage() {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`);
+    }
+  }
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
 
   return (
     <div>
@@ -40,9 +84,19 @@ function LinkList(props) {
           key={link.id}
           showCount={true}
           link={link}
-          index={index + 1}
+          index={index + pageIndex}
         />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPreviousPage}>
+            Previous
+          </div>
+          <div className="pointer" onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 }
